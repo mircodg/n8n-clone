b315b802e0377fcbc135a71743bbc75cddd47a87
Domenico Germanà <Mirkogermana12@gmail.com>
2026-01-02 23:23:10 +0100

    feat: add NodeSelector component to AddNodeButton and extend node type enum in schema.ts

---
+++ components/node-selector.tsx
@@ -0,0 +1,94 @@
+"use client";
+
+import { createId } from "@paralleldrive/cuid2";
+import { useReactFlow } from "@xyflow/react";
+import { GlobeIcon, MousePointerIcon } from "lucide-react";
+import { useCallback } from "react";
+import { toast } from "sonner";
+import { NodeType } from "@/drizzle/schema";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { Separator } from "./ui/separator";
+
+export type NodeTypeOption = {
+  type: (typeof NodeType)[keyof typeof NodeType];
+  label: string;
+  description: string;
+  icon: React.ComponentType<{ className?: string }> | string;
+};
+
+const triggerNodes: NodeTypeOption[] = [
+  {
+    type: NodeType.MANUAL_TRIGGER,
+    label: "Manual Trigger",
+    description:
+      "Trigger the workflow manually. Good for getting started quickly.",
+    icon: MousePointerIcon,
+  },
+];
+
+const executionNodes: NodeTypeOption[] = [
+  {
+    type: NodeType.HTTP_REQUEST,
+    label: "HTTP Request",
+    description: "Make an HTTP request to an external API.",
+    icon: GlobeIcon,
+  },
+];
+
+interface NodeSelectorProps {
+  open: boolean;
+  onOpenChange: (open: boolean) => void;
+  children: React.ReactNode;
+}
+
+export const NodeSelector = ({
+  open,
+  onOpenChange,
+  children,
+}: NodeSelectorProps) => {
+  return (
+    <Sheet open={open} onOpenChange={onOpenChange}>
+      <SheetTrigger asChild>{children}</SheetTrigger>
+      <SheetContent side="right" className="w-full sm:max-w-md overflow-y-auto">
+        <SheetHeader>
+          <SheetTitle>Add Node</SheetTitle>
+          <SheetDescription>
+            A trigger is a step that starts your workflow.
+          </SheetDescription>
+        </SheetHeader>
+        <div>
+          {triggerNodes.map((node) => {
+            const Icon = node.icon;
+            return (
+              <div
+                key={node.type}
+                className="w-full justify-start h-auto py-5 px-4 rounded-none cursor-pointer border-l-2 border-transparent hover:border-l-primary"
+                onClick={() => {}}
+              >
+                <div className="flex items-center gap-6 w-full overflow-hidden">
+                  {typeof Icon === "string" ? (
+                    // eslint-disable-next-line @next/next/no-img-element
+                    <img
+                      alt={node.label}
+                      src={Icon}
+                      className="size-5 object-contain rounded-sm "
+                    />
+                  ) : (
+                    <Icon className="size-5" />
+                  )}
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </SheetContent>
+    </Sheet>
+  );
+};
